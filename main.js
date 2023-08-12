fa78e797b48d9bd40a68dd902dd3c0242f1e4e64
AndreAshmeade <andrelashmeade@gmail.com>
2023-08-12 00:04:50 -0400

    .

---
+++ main.js
@@ -0,0 +1,10 @@
+// Toggle Menu
+let navLinks = document.getElementById("navLinks");
+
+let showMenu = () => {
+  navLinks.style.right = "0";
+};
+
+let hideMenu = () => {
+  navLinks.style.right = "-200";
+};
